4e1030ab3a5ecb7c0fada32d2e79835bfe711c64
Uli Hansen <ulrich.hansen@acentic.com>
2018-04-12 13:42:09 +0200

    save

--- Tomcat/conf/setenv.sh
+++ Tomcat/conf/setenv.sh
@@ -1,7 +1,14 @@
-JAVA_OPTS="-Xms1g
-           -Xmx1g
+JAVA_OPTS="-Xms1512M
+           -Xmx1512M
            -XX:+UseConcMarkSweepGC
            -XX:+CMSIncrementalMode
            -XX:+HeapDumpOnOutOfMemoryError
-           -XX:HeapDumpPath=/var/log/tomcat7/"
+           -XX:HeapDumpPath=/var/log/tomcat7/
+           -Dcom.sun.management.jmxremote
+           -Dcom.sun.management.jmxremote.ssl=false
+           -Dcom.sun.management.jmxremote.local.only=false
+           -Dcom.sun.management.jmxremote.authenticate=false
+           -Dcom.sun.management.jmxremote.port=1098
+           -Dcom.sun.management.jmxremote.rmi.port=1098
+           -Djava.rmi.server.hostname=acs.acentic.com"
 
